3bb72cc0b6085f38d834bb424efff326c62e8ebc
Emilio <emilio.jimenezlz@udlap.mx>
2025-03-27 09:55:22 -0600

    finished Problema6

--- src/Problema6/ProcessA.java
+++ src/Problema6/ProcessA.java
@@ -1,7 +1,12 @@
 package Problema6;
 
+import Problema5.ConveyorBelt;
+
+import java.util.Random;
+
 public class ProcessA extends Thread {
     private final Printer printer;
+    private final Random rand = new Random();
 
     public ProcessA(Printer printer) {
         this.printer = printer;
@@ -11,7 +16,8 @@ public class ProcessA extends Thread {
     public void run() {
         try {
             for (int i = 0; i < 10; i++) {
-                printer.printMessage("Message from Process A", true);
+                printer.printMessage("Mensaje del proceso A", true);
+                Thread.sleep(rand.nextInt(2000));
             }
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
--- src/Problema6/ProcessB.java
+++ src/Problema6/ProcessB.java
@@ -1,7 +1,12 @@
 package Problema6;
 
+import Problema5.Producer;
+
+import java.util.Random;
+
 public class ProcessB extends Thread {
     private final Printer printer;
+    private final Random rand = new Random();
 
     public ProcessB(Printer printer) {
         this.printer = printer;
@@ -11,7 +16,8 @@ public class ProcessB extends Thread {
     public void run() {
         try {
             for (int i = 0; i < 10; i++) {
-                printer.printMessage("Message from Process B", false);
+                printer.printMessage("Mensaje del proceso B", false);
+                Thread.sleep(rand.nextInt(2000));
             }
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
